adb282f482636ead0ef6d45ba1c3ea535069f7a9
AvikNayak <nayakavik19@gmail.com>
2024-10-23 21:53:47 +0530

    destroySession and checkAuth server actions implemented

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/images/logo.png";
 import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import destroySession from "@/app/actions/destroySession";
+import checkAuth from "@/app/actions/checkAuth";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  const router = useRouter();
+
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const fetchAuthStatus = async () => {
+      const result = await checkAuth();
+      setIsAuthenticated(result.isAuthenticated);
+    };
+
+    fetchAuthStatus();
+  }, []);
+
+  const handleLogout = async () => {
+    const { success, error } = await destroySession();
+
+    if (success) {
+      router.push("/login");
+    } else {
+      toast.error(error);
+    }
+  };
+
   return (
     <header className="bg-white shadow-sm">
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -27,47 +57,60 @@ const Header = () => {
                 >
                   Rooms
                 </Link>
-                <Link
-                  href="/bookings"
-                  className="rounded-md px-4 py-2 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-emerald-500 hover:text-white"
-                >
-                  Bookings
-                </Link>
-                <Link
-                  href="/rooms/add"
-                  className="rounded-md px-4 py-2 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-emerald-500 hover:text-white"
-                >
-                  Add Room
-                </Link>
+                {isAuthenticated && (
+                  <>
+                    <Link
+                      href="/bookings"
+                      className="rounded-md px-4 py-2 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-emerald-500 hover:text-white"
+                    >
+                      Bookings
+                    </Link>
+                    <Link
+                      href="/rooms/add"
+                      className="rounded-md px-4 py-2 text-sm font-semibold text-gray-700 transition duration-300 hover:bg-emerald-500 hover:text-white"
+                    >
+                      Add Room
+                    </Link>
+                  </>
+                )}
               </div>
             </div>
           </div>
           <div className="ml-auto">
             <div className="ml-4 flex items-center md:ml-6 space-x-4">
-              <Link
-                href="/login"
-                className="text-gray-700 transition duration-300 hover:text-emerald-500"
-              >
-                <FaSignInAlt className="inline mr-2 text-lg" /> Login
-              </Link>
-              <Link
-                href="/register"
-                className="text-gray-700 transition duration-300 hover:text-emerald-500"
-              >
-                <FaUser className="inline mr-2 text-lg" /> Register
-              </Link>
-              <Link
-                href="/rooms/my"
-                className="text-gray-700 transition duration-300 hover:text-emerald-500"
-              >
-                <FaBuilding className="inline mr-2 text-lg" /> My Rooms
-              </Link>
-              <Link
-                href="/login"
-                className="text-gray-700 transition duration-300 hover:text-emerald-500"
-              >
-                <FaSignOutAlt className="inline mr-2 text-lg" /> Sign Out
-              </Link>
+              {!isAuthenticated && (
+                <>
+                  <Link
+                    href="/login"
+                    className="text-gray-700 transition duration-300 hover:text-emerald-500"
+                  >
+                    <FaSignInAlt className="inline mr-2 text-lg" /> Login
+                  </Link>
+                  <Link
+                    href="/register"
+                    className="text-gray-700 transition duration-300 hover:text-emerald-500"
+                  >
+                    <FaUser className="inline mr-2 text-lg" /> Register
+                  </Link>
+                </>
+              )}
+
+              {isAuthenticated && (
+                <>
+                  <Link
+                    href="/rooms/my"
+                    className="text-gray-700 transition duration-300 hover:text-emerald-500"
+                  >
+                    <FaBuilding className="inline mr-2 text-lg" /> My Rooms
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="text-gray-700 transition duration-300 hover:text-emerald-500"
+                  >
+                    <FaSignOutAlt className="inline mr-2 text-lg" /> Sign Out
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
@@ -75,24 +118,28 @@ const Header = () => {
       {/* Mobile menu */}
       <div className="md:hidden">
         <div className="space-y-1 px-4 pb-3 pt-2">
-          <Link
-            href="/"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
-          >
-            Rooms
-          </Link>
-          <Link
-            href="/bookings"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
-          >
-            Bookings
-          </Link>
-          <Link
-            href="/rooms/add"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
-          >
-            Add Room
-          </Link>
+          {isAuthenticated && (
+            <>
+              <Link
+                href="/"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
+              >
+                Rooms
+              </Link>
+              <Link
+                href="/bookings"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
+              >
+                Bookings
+              </Link>
+              <Link
+                href="/rooms/add"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-emerald-500 hover:text-white"
+              >
+                Add Room
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
